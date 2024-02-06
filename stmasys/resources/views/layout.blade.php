8e1b04d3e554c8405f4bd4a4be9bf24f2a420adb
Loyd Ngei <loydngei@gmail.com>
2024-02-07 01:20:32 +0300

    Navbar

--- stmasys/resources/views/layout.blade.php
+++ stmasys/resources/views/layout.blade.php
@@ -10,14 +10,21 @@
         @vite('resources/css/app.css')
     </head>
     <body>
-        <nav class="">
-            <ul class="">
-                <li>Home</li>
-                <li>About</li>
-                <li>Search</li>
-                <li>Register</li>
+        <nav class="flex bg-black h-50 text-white">
+            <div>
+                <a href="/">
+                    <img src="{{asset('images/Ecosa.jpg')}}" class="h-24 w-30 object-contain p-2" alt="Logo"/>
+                </a>
+            </div>
+            <ul class="flex space-x-10 p-4">
+                <li><a href="#" class="hover:bg-yellow-600 px-2 py-1 rounded">Home</a></li>
+                <li><a href="#" class="hover:bg-yellow-600 px-2 py-1 rounded">About</a></li>
+                <li><a href="#" class="hover:bg-yellow-600 px-2 py-1 rounded">Search</a></li>
+                <li><a href="#" class="hover:bg-yellow-600 px-2 py-1 rounded">Register</a></li>
             </ul>
         </nav>
+
+
     </body>
 
 </html>

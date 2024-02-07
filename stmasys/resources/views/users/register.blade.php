0d90b0e5cccd74247637834e329ba71ff5a9109a
Loyd Ngei <loydngei@gmail.com>
2024-02-08 00:14:31 +0300

    Created register.blade file

---
+++ stmasys/resources/views/users/register.blade.php
@@ -0,0 +1,10 @@
+<form method="POST" action="/users/authenticate">
+    <div>
+        <label for="name"></label>
+        <input placeholder="Registration Number"/>
+    </div>
+    <div>
+        <label for="name">password</label>
+        <input placeholder="password"/>
+    </div>
+</form>

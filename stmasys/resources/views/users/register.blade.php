60fbd557c69ddd6a7bf05d8c7f0735189bc53d06
Loyd Ngei <loydngei@gmail.com>
2024-02-09 23:43:15 +0300

    Register.blade

--- stmasys/resources/views/users/register.blade.php
+++ stmasys/resources/views/users/register.blade.php
@@ -1,10 +1,24 @@
-<form method="POST" action="/users/authenticate">
-    <div>
-        <label for="name"></label>
-        <input placeholder="Registration Number"/>
+<x-layout>
+    <div class="min-h-screen flex items-center justify-center">
+        <form method="POST" action="/users" class="bg-blue-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            @csrf
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="registration_number">
+                    Registration Number
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="registration_number" type="text" placeholder="Registration Number">
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
+            </div>
+            <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Sign In
+                </button>
+            </div>
+        </form>
     </div>
-    <div>
-        <label for="name">password</label>
-        <input placeholder="password"/>
-    </div>
-</form>
+</x-layout>

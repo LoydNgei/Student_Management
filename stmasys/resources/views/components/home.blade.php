665544426fcc849ea38bb3afc83c5e658d3d91d3
Loyd Ngei <loydngei@gmail.com>
2024-02-09 23:49:44 +0300

    Created home.blade

---
+++ stmasys/resources/views/components/home.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <div class="flex space-between h-16 w-24">
+        @foreach ($lecturers as $lecturer)
+            <div>
+                <img src="" alt="">
+                <p><strong>Lecturer Name: </strong>Dr Martin Oleche</p>
+                <p></p>
+            </div>  
+            
+        @endforeach       
+    </div>
+</x-layout>

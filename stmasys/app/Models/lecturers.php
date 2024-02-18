97a8fded34ab9bed06af906a5ce0c81a8f75dfc8
Loyd Ngei <loydngei@gmail.com>
2024-02-18 23:55:25 +0300

    Lecturer model

--- stmasys/app/Models/lecturers.php
+++ stmasys/app/Models/lecturers.php
@@ -5,12 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class lecturers extends Model
+class Lecturer extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'Lecturer Name',
-        'Course taught'
+        'lecturer_name',
+        'course_taught'
     ];
+
+    public static function createDummyData()
+    {
+        // Dummy data for 9 lecturers
+        $dummyData = [
+            ['lecturer_name' => 'John Doe', 'course_taught' => 'Computer Science'],
+            ['lecturer_name' => 'Jane Smith', 'course_taught' => 'Mathematics'],
+        ];
+    }
 }

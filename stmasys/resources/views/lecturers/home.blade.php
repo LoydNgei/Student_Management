44880d1ba07d4376574277250d082fcc87ee8681
Loyd Ngei <loydngei@gmail.com>
2024-02-21 22:45:47 +0300

    home.blade.php

---
+++ stmasys/resources/views/lecturers/home.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <x-navbar>
+        <div class="grid grid-cols-3 gap-4">
+            {{-- @foreach ($lecturers as $lecturer) --}}
+                <div class="border p-4">
+                    {{-- <img src="{{ $lecturer->image }}" alt="{{ $lecturer->name }}">
+                    <p><strong>Lecturer Name:</strong> {{ $lecturer->name }}</p>
+                    <p><strong>Email:</strong> {{ $lecturer->email }}</p> --}}
+                    <!-- Add more details as needed -->
+                </div>
+            {{-- @endforeach --}}
+        </div>
+    </x-navbar>
+</x-layout>    

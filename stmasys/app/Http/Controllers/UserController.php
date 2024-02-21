849945aa3af1395d1b7a0d4c9241f29289759d93
Loyd Ngei <loydngei@gmail.com>
2024-02-21 21:53:56 +0300

    User controller

--- stmasys/app/Http/Controllers/UserController.php
+++ stmasys/app/Http/Controllers/UserController.php
@@ -1,42 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     // Create Form
 
-    public function create() {
+    public function showform() {
         return view('users.register');
     }
 
-    // Create New User
-
-    public function store(Request $request) {
+    public function login(Request $request) {
         $formFields = $request->validate([
-            'name' => ['required', 'min:3'],
-            'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => 'required|confirmed|min:6'
+            'Registration Number' => 'required',
+            'password' => 'required' 
         ]);
 
-        // Hash Password
-
-        $formFields['password'] = bcrypt($formFields['password']);
-
-
-        // Create User
-
-        $user = User::create($formFields);
-
-        // Login User
-
-        auth()->login($user);
-
-        return redirect('/')->with('message', 'User created and logged in');
-
+        if(auth()->attempt($formFields)) {
+            return redirect('/')->with('message', 'You are now logged in');
+        }
+        return back()->withErrors('Invalid credentials');
 
     }
-    
+
+
 }

f070eda3ad9ba691e874c4eeb8d5eb2b95a03a06
Loyd Ngei <loydngei@gmail.com>
2024-02-21 22:46:23 +0300

    Styling changes

--- stmasys/resources/views/users/register.blade.php
+++ stmasys/resources/views/users/register.blade.php
@@ -3,21 +3,22 @@
         <form method="POST" action="/users" class="bg-blue-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
             @csrf
             <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="registration_number">
+                <label class="block text-sm font-bold mb-2" for="registration_number">
                     Registration Number
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="registration_number" type="text" placeholder="Registration Number">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="registration_number" type="text" placeholder="Registration Number">
             </div>
             <div class="mb-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     Password
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
             </div>
+
             <div class="flex items-center justify-between">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                    Sign In
-                </button>
+                <a href="{{ route('home') }}" class="bg-blue-500 text-white font-bold py-2 px-4 hover:bg-blue-700 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    Log in
+                </a>
             </div>
         </form>
     </div>

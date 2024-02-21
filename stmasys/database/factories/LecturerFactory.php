a93c8b57f05e6f512d754315e83e9116c1bea9b2
Loyd Ngei <loydngei@gmail.com>
2024-02-21 22:43:25 +0300

    Created Lecturer Factory

---
+++ stmasys/database/factories/LecturerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class LecturerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'lecturer_name' => $this->faker->name,
+            'course_taught' => $this->faker->sentence(),
+        ];
+    }
+}

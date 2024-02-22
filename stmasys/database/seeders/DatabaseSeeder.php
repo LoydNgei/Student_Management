900c629be37a1687534658050ffc08e2978cf6c3
Loyd Ngei <loydngei@gmail.com>
2024-02-22 22:24:33 +0300

    Database seeder

--- stmasys/database/seeders/DatabaseSeeder.php
+++ stmasys/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
-use App\Models\Lecturers;
+use App\Models\Lecturer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        Lecturers::factory(10)->create();
+        Lecturer::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',

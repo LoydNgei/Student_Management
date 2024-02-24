76f1d56c3c4f8a50b78669246e9bb0e7d5987426
Loyd Ngei <loydngei@gmail.com>
2024-02-24 22:55:09 +0300

    Added the lecturers args in home.blade.php

--- stmasys/resources/views/lecturers/home.blade.php
+++ stmasys/resources/views/lecturers/home.blade.php
@@ -2,11 +2,11 @@
     <x-navbar>
         <div class="grid grid-cols-3 gap-4">
             @foreach ($lecturers as $lecturer)
-                <div class="border p-4">
+                {{-- <div class="border p-4">
                     <img src="{{ $lecturer->image }}" alt="{{ $lecturer->name }}">
-                    <p><strong>Lecturer Name:</strong> {{ $lecturer->name }}</p>
-                    <p><strong>Email:</strong> {{ $lecturer->email }}</p>
-                </div>
+                    <p><strong>Lecturer Name:</strong> {{ $lecturer->lecturer_name }}</p>
+                    <p><strong>Email:</strong> {{ $lecturer->course_taught }}</p>
+                </div> --}}
             @endforeach
         </div>
     </x-navbar>

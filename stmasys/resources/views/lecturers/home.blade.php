dc7cd4f3f9dd17721416f6ae650dcdcaccfbcfe6
Loyd Ngei <loydngei@gmail.com>
2024-02-25 23:19:46 +0300

    Home.blade.php

--- stmasys/resources/views/lecturers/home.blade.php
+++ stmasys/resources/views/lecturers/home.blade.php
@@ -1,13 +1,14 @@
+{{-- {{ dd($lecturers) }} --}}
+
 <x-layout>
     <x-navbar>
         <div class="grid grid-cols-3 gap-4">
             @foreach ($lecturers as $lecturer)
-                {{-- <div class="border p-4">
-                    <img src="{{ $lecturer->image }}" alt="{{ $lecturer->name }}">
+                <div class="border p-4">
                     <p><strong>Lecturer Name:</strong> {{ $lecturer->lecturer_name }}</p>
-                    <p><strong>Email:</strong> {{ $lecturer->course_taught }}</p>
-                </div> --}}
+                    <p><strong>Course Taught:</strong> {{ $lecturer->course_taught }}</p>
+                </div>
             @endforeach
         </div>
     </x-navbar>
-</x-layout>    
+</x-layout>

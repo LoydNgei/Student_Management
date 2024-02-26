5799b85d0a19b338bef81feb0bc2c78e878e37b1
Loyd Ngei <loydngei@gmail.com>
2024-02-26 22:35:53 +0300

    Styling to the register file

--- stmasys/resources/views/users/register.blade.php
+++ stmasys/resources/views/users/register.blade.php
@@ -7,8 +7,7 @@
                     Registration Number
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="registration_number" type="text" placeholder="Registration Number">
-            </div>
-            <div class="mb-6">
+    
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     Password
                 </label>

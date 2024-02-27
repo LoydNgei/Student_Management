446014a5363b64b8bbff34e91949ecf50476d016
Loyd Ngei <loydngei@gmail.com>
2024-02-27 16:07:04 +0300

    added more factories

--- stmasys/database/seeders/DatabaseSeeder.php
+++ stmasys/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Lecturer::factory(10)->create();
+        Lecturer::factory(16)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',

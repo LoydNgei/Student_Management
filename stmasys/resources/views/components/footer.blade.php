eac23743a1a7b765256f2a8b2b3d4f5e6bc16162
Loyd Ngei <loydngei@gmail.com>
2024-02-27 16:07:25 +0300

    Created footer.blade.php file

---
+++ stmasys/resources/views/components/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="bottom-0 w-full flex justify-between bg-blue-950 text-white">
+    {{-- Left List --}}
+    <div class="m-5">
+        <ul class="space-y-3">
+            <li>University of Nairobi</li>
+            <li>P. O. Box 30197 00100 Nairobi-Kenya</li>
+        </ul>
+    </div>
+    {{-- Right List --}}
+    <div class="m-5">
+        <ul>
+            <li><i class="fas fa-envelope"></i> <span>ecosa@uonbi.ac.ke</span></li>
+            <div class="space-x-5 space-y-5">
+                <i class="fab fa-facebook hover:bg-blue-600 rounded"></i>
+                <i class="fab fa-twitter hover:bg-blue-600 rounded"></i>
+                <i class="fas fa-globe hover:bg-blue-600 rounded"></i>
+            </div>
+        </ul>
+    </div>
+</footer>

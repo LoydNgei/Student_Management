45e01022d7477ce2a24000762e1f777a98547655
Loyd Ngei <loydngei@gmail.com>
2024-03-26 16:02:37 +0300

    Register blade

--- stmasys/resources/views/users/register.blade.php
+++ stmasys/resources/views/users/register.blade.php
@@ -1,13 +1,14 @@
 <x-layout>
     <div class="min-h-screen flex items-center justify-center">
-        <form method="POST" action="/users" class="bg-blue-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form method="POST" action="/login" class="bg-blue-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+
             @csrf
             <div class="mb-4">
                 <label class="block text-sm font-bold mb-2" for="registration_number">
                     Registration Number
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="registration_number" type="text" placeholder="Registration Number">
-    
+
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     Password
                 </label>

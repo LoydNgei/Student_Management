d0f582071df83716878fab9632c971e43eb321e6
LoydNgei <loydngei@gmail.com>
2024-04-22 17:53:06 +0300

    Added course Navigation

--- stmasys/resources/views/lecturers/home.blade.php
+++ stmasys/resources/views/lecturers/home.blade.php
@@ -1,13 +1,30 @@
 <x-layout>
     <x-navbar></x-navbar>
-        <div class="grid grid-cols-3 gap-4 pt-32">
-            @foreach ($lecturers as $lecturer)
-                <div class="border p-4">
-                    <p><strong>Lecturer Name:</strong> {{ $lecturer->lecturer_name }}</p>
-                    <p><strong>Course Taught:</strong> {{ $lecturer->course_taught }}</p>
-                    <a href="{{ route('inquiry') }}">Submit Query</a>
-                </div>
-            @endforeach
+
+    <!-- Grid layout -->
+    <div class="grid grid-cols-3 gap-4 pt-32">
+        <!-- Lecturers section (3/4 of the width) -->
+        <div class="col-span-2">
+            <div class="grid grid-cols-3 gap-4">
+                @foreach ($lecturers as $lecturer)
+                    <div class="border p-4 mb-4">
+                        <p><img src="stmasys/public/images/Ecosa.jpg" alt="">{{ $lecturer->image }}</p>
+                        <p><strong>Lecturer Name:</strong> {{ $lecturer->lecturer_name }}</p>
+                        <p><strong>Course Taught:</strong> {{ $lecturer->course_taught }}</p>
+                        <a href="{{ route('inquiry') }}">Submit Query</a>
+                    </div>
+                @endforeach
+            </div>
+
+            {{ $lecturers->links() }}
+
         </div>
+
+        <!-- Dropdown menu (1/4 of the width) -->
+        <div class="col-span-1">
+            @include('courseNav')
+        </div>
+    </div>
 </x-layout>
+
 <x-footer></x-footer>

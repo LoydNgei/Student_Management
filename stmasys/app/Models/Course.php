180cedae4c85b7fc12b0ce249aa8dd18982a667a
LoydNgei <loydngei@gmail.com>
2024-04-29 22:40:08 +0300

    Cleared the course model

--- stmasys/app/Models/Course.php
+++ stmasys/app/Models/Course.php
@@ -1,11 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-// class Course extends Model
-// {
-//     protected $fillable = [];
-// } 

91c4fbc3d862e6c4a29feb633d6be8759bdbe865
LoydNgei <loydngei@gmail.com>
2024-05-03 20:35:57 +0300

    changed tthe naming of the routes

--- stmasys/app/Http/Controllers/UserController.php
+++ stmasys/app/Http/Controllers/UserController.php
@@ -3,58 +3,65 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     // Show Login Form
-
-    public function showLoginForm() {
+    public function login() {
         return view('users.login');
     }
 
-
     // Show Register Form
-
-    public function showRegistrationForm() {
+    public function registration() {
         return view('users.register');
-    }
-
-
-    // Register User
-
-    public function registerForm(Request $request) {
-        $formFields = $request->validate([
-            'registration_number' => 'required',
-            'password' => 'required',
-            'confirm_password' => 'required'
-        ]);
-
-
-        // Create a new User instance
-
-        $user = new User();
-        $user->registration_number = $formFields['registration_number'];
-        $user->password = bcrypt($formFields['password']);
-        $user->confirm_password = $formFields['confirm_password'];
-        $user->save();
-
-        // Redirect the user after registration
-        return redirect()->route('login')->with('message', 'User Registered successfully');
     }
 
 
     // Log in process
 
-    public function loginForm(Request $request) {
+    public function loginPost(Request $request) {
         $formFields = $request->validate([
             'registration_number' => 'required',
             'password' => 'required'
         ]);
 
         if(auth()->attempt($formFields)) {
-            return redirect('/')->with('message', 'You are now logged in');
+            return redirect('/home');
         }
-        return back()->withErrors('Invalid credentials');
+        return back('/login')->withErrors('Invalid credentials');
+    }
 
+
+    // Register User
+    public function registrationPost(Request $request) {
+        $formFields = $request->validate([
+            'registration_number' => 'required',
+            'password' => 'required',
+            'confirm_password' => 'required'
+        ]);
+
+        // Create a new User instance
+        $user = new User();
+        $user->registration_number = $formFields['registration_number'];
+        $user->password = Hash::make($formFields['password']);
+        $user->confirm_password = $formFields['confirm_password'];
+        $user->save();
+
+        if (!$user) {
+            return back('/register')->withErrors('Registration failed. Try Again');
+        }
+        return redirect()->route('login')->with('message', 'User Registered successfully');
+    }
+
+
+
+    // Logout
+
+    public function logout() {
+        session:flush();
+        Auth::logout();
+        return redirect('/login');
     }
 }

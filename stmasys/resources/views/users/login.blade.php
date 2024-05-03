8c508020288f37450e960b0dac59adeb26d63600
LoydNgei <loydngei@gmail.com>
2024-05-03 20:38:59 +0300

    Added correct routing

--- stmasys/resources/views/users/login.blade.php
+++ stmasys/resources/views/users/login.blade.php
@@ -1,7 +1,6 @@
 <x-layout>
     <div class="min-h-screen flex items-center justify-center">
-        <form method="POST" action="{{ route('login') }}" class="bg-blue-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-
+        <form method="POST" action="{{ route('login.post') }}" class="bg-blue-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
             @csrf
             <div class="mb-4">
                 <label class="block text-sm font-bold mb-2" for="registration_number">
@@ -16,7 +15,7 @@
             </div>
 
             <div class="flex items-center justify-between">
-                <a href="{{ route('home') }}" class="bg-blue-500 text-white font-bold py-2 px-4 hover:bg-blue-700 rounded focus:outline-none focus:shadow-outline" type="submit">
+                <a href="{{route('login')}}" class="bg-blue-500 text-white font-bold py-2 px-4 hover:bg-blue-700 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Log in
                 </a>
             </div>
